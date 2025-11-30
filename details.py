bde9419123b8b2522cc0f491667b8855664b4c98
Jitu626 <ritudev9211@gmail.com>
2025-11-30 19:39:59 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,5 @@
-api_id = 27881923
-api_hash = "79abda5e46a51fc0dce1313f2548ce19"
-bot_token = "6263666911:AAF_Zm2baKNCJMGPn8aCBlVArvTa-A4kz1k"
+api_id = 39321121
+api_hash = "ba3cb13f2ff33ed53c28955e75d7ede1"
+bot_token = "8577871553:AAHUDsoByXP4uh-VD7QU8E99EuX-YUSyJa0"
+
 
